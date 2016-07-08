db1012f2b8e56ce140bbe736ef6031e9e5b977d7
Ernesto Nobmann <nobmann88@gmail.com>
2016-07-08 17:35:28 -0500

    Heroku Config - Part 2
    * Updating requirements.txt
    * Fixing .wsgi.py file

--- ideam_cdc/wsgi.py
+++ ideam_cdc/wsgi.py
@@ -10,10 +10,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideam_cdc.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+try:
+    from dj_static import Cling
+
+    application = Cling(get_wsgi_application())
+except:
+    pass
