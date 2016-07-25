ede7481872a2865a40ab567e7f31fb6f3ff77e33
Ernesto Nobmann <nobmann88@gmail.com>
2016-07-24 19:48:27 -0500

    Custom UserProfile
    * Adding analyst and developer custom fields

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -5,9 +5,10 @@ from models import UserProfile
 class SignupForm(forms.ModelForm):
 	class Meta:
 		model = UserProfile
-		fields = ('mobile_number',)
+		fields = ('is_analyst', 'is_developer')
 
 	def save(self, user):
 		profile = UserProfile(user=user)
-		profile.mobile_number = self.cleaned_data['mobile_number']
+		profile.is_analyst = self.cleaned_data['is_analyst']
+		profile.is_developer = self.cleaned_data['is_developer']
 		profile.save()
--- user_profile/models.py
+++ user_profile/models.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-	# user = models.ForeignKey(User, unique=True)
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	mobile_number = models.CharField(max_length=30, blank=True, null=True)
-
-
-# User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+	is_analyst = models.BooleanField(default=False)
+	is_developer = models.BooleanField(default=False)
