cd0431d3710a147ee013022d127fe87fe5eb815e
Ernesto Nobmann <nobmann88@gmail.com>
2016-08-05 17:39:53 -0500

    CEOS - CDCOL - Storage Units
    * Added list storage units
    * Added create a new storage unit type CEOS
    * Added create a new storage unit type CDCOL
    * urls.py, views.py created and modified to match the urls

--- storage/urls.py
+++ storage/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 app_name = 'storage'
 urlpatterns = [
-	# ex: /items/
+	# ex: /storage/
 	url(r'^$', views.index, name='index'),
+	# ex: /storage/detail/11
+	url(r'^detail/(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
+	# ex: /storage/ceos/new
+	url(r'^ceos/new/$', views.new_ceos, name='new_ceos'),
+	# ex: /storage/cdcol/new
+	url(r'^cdcol/new/$', views.new_cdcol, name='new_cdcol'),
 ]
--- storage/views.py
+++ storage/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 def index(request):
 	current_user = request.user
 	return render(request, 'storage/index.html')
+
+
+def detail(request, item_id):
+	print item_id
+	current_user = request.user
+	return render(request, 'storage/detail.html')
+
+
+def new_ceos(request):
+	current_user = request.user
+	return render(request, 'storage/new_ceos.html')
+
+
+def new_cdcol(request):
+	current_user = request.user
+	return render(request, 'storage/new_cdcol.html')
