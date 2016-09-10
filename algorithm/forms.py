cfd44d98d6a054f4ecb01f594ab45a8b5605f888
Ernesto Nobmann <nobmann88@gmail.com>
2016-09-09 22:56:08 -0500

    Algorithm Creation Fixex
    * Fixing the forms
    * Beginning reading the response from the user
    * Template fixes

--- algorithm/forms.py
+++ algorithm/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from algorithm.models import Topic
 
 
 class AlgorithmForm(forms.Form):
-	topic = forms.CharField(max_length=200, required=True)
+	topic = forms.ModelChoiceField(queryset=Topic.objects.all(), required=True)
 	name = forms.CharField(max_length=200, required=True)
 	description = forms.CharField(widget=forms.Textarea, required=True)
 	source_storage_units = forms.CharField(max_length=200, required=True)
--- algorithm/views.py
+++ algorithm/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from algorithm.models import Algorithm, Topic
+from storage.models import StorageUnit
+from algorithm.forms import AlgorithmForm
 
 @login_required(login_url='/accounts/login/')
 def index(request):
@@ -11,7 +13,29 @@ def index(request):
 @login_required(login_url='/accounts/login/')
 def new(request):
 	current_user = request.user
-	return render(request, 'algorithm/new.html')
+	topics = Topic.objects.all()
+	source_storage_units = StorageUnit.objects.all()
+	if request.method == 'POST':
+		# getting the form
+		algorithm_form = AlgorithmForm(request.POST)
+		# checking if the form is valid
+		if algorithm_form.is_valid():
+			field_topic = algorithm_form.cleaned_data['topic']
+			field_name = algorithm_form.cleaned_data['name']
+			field_description = algorithm_form.cleaned_data['description']
+			field_source_storage_unit = algorithm_form.cleaned_data['source_storage_unit']
+			field_output_storage_unit = algorithm_form.cleaned_data['output_storage_unit']
+
+			## TODO: Read the response and create the object
+
+			# print field_topic
+			# return render(request, 'algorithm/index.html')
+		else:
+			algorithm_form.add_error(None, "Favor completar todos los campos marcados.")
+	else:
+		algorithm_form = AlgorithmForm()
+	context = {'algorithm_form': algorithm_form, 'topics': topics, 'source_storage_units': source_storage_units}
+	return render(request, 'algorithm/new.html', context)
 
 
 @login_required(login_url='/accounts/login/')
