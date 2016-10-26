654fbac2eced42d9f0383dd92c5996453f7b8c12
Ernesto Nobmann <nobmann88@gmail.com>
2016-10-26 14:40:45 -0500

    Latlong value approximation added again xD

--- ideam_cdc/static_dirs/js/formBuilder.js
+++ ideam_cdc/static_dirs/js/formBuilder.js
@@ -65,10 +65,10 @@ $(document).ready(function () {
         var ne = rectangle.getBounds().getNorthEast();
         var sw = rectangle.getBounds().getSouthWest();
 
-        document.getElementById("sw_latitude").value = ne.lat();
-        document.getElementById("sw_longitude").value = sw.lng();
-        document.getElementById("ne_latitude").value = sw.lat();
-        document.getElementById("ne_longitude").value = ne.lng();
+        document.getElementById("sw_latitude").value = Math.ceil(sw.lng());
+        document.getElementById("sw_longitude").value = Math.floor(ne.lat());
+        document.getElementById("ne_latitude").value = Math.floor(ne.lat());
+        document.getElementById("ne_longitude").value = Math.ceil(ne.lng());
 
     }
     
