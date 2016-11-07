eb63a5b87cee4f62eccd46f4ba13462901ad3478
Ernesto Nobmann <nobmann88@gmail.com>
2016-11-07 12:01:28 -0500

    YamlTemplate improvement
    * Added Type
    * Modified admin template

--- template/admin.py
+++ template/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from template.models import *
 
-admin.site.register(YamlTemplate)
+
+class YamlAdmin(admin.ModelAdmin):
+	list_display = ('name', 'type', 'created_at')
+	# search_fields = ['name']
+	admin.ModelAdmin.ordering = ('-created_at', )
+	admin.ModelAdmin.list_per_page = 20
+
+admin.site.register(YamlTemplate, YamlAdmin)
--- template/models.py
+++ template/models.py
@@ -1,10 +1,21 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 
 class YamlTemplate(models.Model):
+	DESCRIPTION_TYPE = '1'
+	INGEST_TYPE = '2'
+	TYPES = (
+		(DESCRIPTION_TYPE, "DESCRIPCIÓN"),
+		(INGEST_TYPE, "INGESTA"),
+	)
 	name = models.CharField(max_length=200)
 	file = models.FileField()
+	type = models.CharField(max_length=2, choices=TYPES)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return "{} - {} - {}".format(self.name, self.get_type_display(), self.created_at)
