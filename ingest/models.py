7a2f98b571b4b7a4ec696627723eec26cd62f7fd
Ernesto Nobmann <nobmann88@gmail.com>
2016-11-16 15:29:01 -0500

    Creating clean new migrations

--- ingest/models.py
+++ ingest/models.py
@@ -20,7 +20,7 @@ class IngestTask(models.Model):
 	storage_unit = models.ForeignKey(StorageUnit, on_delete=models.CASCADE, related_name='ingest_storage_unit')
 	state = models.CharField(max_length=2, choices=INGEST_STATES)
 	comments = models.TextField()
-	errors = models.TextField()
+	error_messages = models.TextField()
 	logs = models.TextField()
 	start_execution_date = models.DateField(null=True, blank=True)
 	end_execution_date = models.DateField(null=True, blank=True)
