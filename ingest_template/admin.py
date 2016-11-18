cea94f1fedfd0811a76b25d7a141e5d117588e9c
Ernesto Nobmann <nobmann88@gmail.com>
2016-11-17 22:49:13 -0500

    Temporary solution to the admin view bug

--- ingest_template/admin.py
+++ ingest_template/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from ingest_template.models import *
 
 
-class IngestTemplateAdmin(admin.ModelAdmin):
-	list_display = ('name', 'created_at', 'updated_at')
-	admin.ModelAdmin.ordering = ('-created_at', )
-	admin.ModelAdmin.list_per_page = 20
+# class IngestTemplateAdmin(admin.ModelAdmin):
+# 	list_display = ('name', 'created_at', 'updated_at')
+# 	admin.ModelAdmin.ordering = ('-created_at', )
+# 	admin.ModelAdmin.list_per_page = 20
+#
+# admin.site.register(IngestTemplate, IngestTemplateAdmin)
 
-admin.site.register(IngestTemplate, IngestTemplateAdmin)
+admin.site.register(IngestTemplate)
--- template/admin.py
+++ template/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from template.models import *
 
 
-class YamlAdmin(admin.ModelAdmin):
-	list_display = ('name', 'type', 'created_at')
-	# search_fields = ['name']
-	admin.ModelAdmin.ordering = ('-created_at', )
-	admin.ModelAdmin.list_per_page = 20
-	# readonly_fields = ('id', 'type', 'name', 'file', 'updated_at')
+# class YamlAdmin(admin.ModelAdmin):
+# 	list_display = ('name', 'type', 'created_at')
+# 	# search_fields = ['name']
+# 	admin.ModelAdmin.ordering = ('-created_at', )
+# 	admin.ModelAdmin.list_per_page = 20
+# 	# readonly_fields = ('id', 'type', 'name', 'file', 'updated_at')
 
-admin.site.register(YamlTemplate, YamlAdmin)
+# admin.site.register(YamlTemplate, YamlAdmin)
+admin.site.register(YamlTemplate)
