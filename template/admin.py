f77056eaab0a052ca292de055fd493949fc784e4
Ernesto Nobmann <nobmann88@gmail.com>
2016-12-13 21:06:54 -0500

    Se comentan lineas de admin

--- template/admin.py
+++ template/admin.py
@@ -9,21 +9,21 @@ class EventAdmin(admin.ModelAdmin):
 	search_fields = ['name', 'type']
 	list_filter = ('type', 'created_at')
 
-	def get_readonly_fields(self, request, obj=None):
-		"""
-		overriding the readonly_fields method
-		"""
-		if request.user.groups.filter(name='DataAdmin').exists():
-			self.readonly_fields = self.dynamic_readonly_fields()
-		return super(EventAdmin, self).get_readonly_fields(request, obj)
-
-	def dynamic_readonly_fields(self):
-		"""
-		setting the readonly_fields
-		based on http://stackoverflow.com/a/31787817/4808337 answer
-		"""
-		readonly_fields = ('id', 'name', 'type', 'file', 'created_at', 'updated_at')
-		return readonly_fields
+	# def get_readonly_fields(self, request, obj=None):
+	# 	"""
+	# 	overriding the readonly_fields method
+	# 	"""
+	# 	if request.user.groups.filter(name='DataAdmin').exists():
+	# 		self.readonly_fields = self.dynamic_readonly_fields()
+	# 	return super(EventAdmin, self).get_readonly_fields(request, obj)
+	#
+	# def dynamic_readonly_fields(self):
+	# 	"""
+	# 	setting the readonly_fields
+	# 	based on http://stackoverflow.com/a/31787817/4808337 answer
+	# 	"""
+	# 	readonly_fields = ('id', 'name', 'type', 'file', 'created_at', 'updated_at')
+	# 	return readonly_fields
 
 
 admin.site.register(YamlTemplate, EventAdmin)
