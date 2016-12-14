316898f4f29307be32b84c4067e118f509408b1e
Ernesto Nobmann <nobmann88@gmail.com>
2016-12-13 21:25:48 -0500

    Modo producción on

--- ideam_cdc/settings.py
+++ ideam_cdc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$1%5%5d+d5*zzi!f4h#$rqx@rvzo@h)mnfmze-fsn-6wg(=&tv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.106.11']
 
