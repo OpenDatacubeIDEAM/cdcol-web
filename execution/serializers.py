3ec52610486d5852e960f8e08c8e423aa655e076
Ernesto Nobmann <nobmann88@gmail.com>
2017-01-07 16:50:09 -0500

    Corrección 4: Ejecuciones en espera
    * Se agrega tooltip cuando el estado es “En Espera” o “En ejecución” el cual muestra el número de ejecuciones en este estado.

--- execution/serializers.py
+++ execution/serializers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
 from execution.models import Execution
+from django.db.models import Avg, Q
 
 
 class ExecutionSerializer(serializers.ModelSerializer):
@@ -9,13 +10,17 @@ class ExecutionSerializer(serializers.ModelSerializer):
 	created_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S")
 	started_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S")
 	finished_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S")
+	current_executions = serializers.SerializerMethodField()
 
 	class Meta:
 		model = Execution
-		fields = ('id', 'algorithm_name', 'state', 'created_at', 'started_at', 'finished_at')
+		fields = ('id', 'algorithm_name', 'state', 'created_at', 'started_at', 'finished_at', 'current_executions')
 
 	def get_algorithm_name(self, obj):
 		return obj.version.algorithm.name
 
 	def get_state(self, obj):
 		return obj.get_state_display()
+
+	def get_current_executions(self, obj):
+		return Execution.objects.filter(Q(state=Execution.ENQUEUED_STATE) | Q(state=Execution.EXECUTING_STATE)).count()
