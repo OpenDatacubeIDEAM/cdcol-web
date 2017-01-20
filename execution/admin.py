5eb960f4239d0f388097ebb92c50c1c15d66dd8f
Ernesto Nobmann <nobmann88@gmail.com>
2017-01-20 09:58:33 -0500

    Actualizaciones de admin
    * Se actualizan interfaces de admin para mostrar mejor los datos

--- execution/admin.py
+++ execution/admin.py
@@ -1,7 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from execution.models import *
 
-admin.site.register(Execution)
+
+class ExecutionAdmin(admin.ModelAdmin):
+	list_display = (
+	'version', 'state', 'created_at', 'results_available', 'results_deleted_at', 'email_sent', 'started_at',
+	'finished_at')
+	ordering = ('-created_at',)
+	list_filter = ('state', 'results_available', 'created_at', 'email_sent', 'started_at', 'finished_at')
+
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ('execution', 'state', 'uuid', 'created_at', 'updated_at', 'start_date', 'end_date', 'state_updated_at')
+	ordering = ('-created_at',)
+	list_filter = ('state', 'uuid', 'created_at', 'updated_at', 'start_date', 'end_date', 'state_updated_at')
+
+
+admin.site.register(Execution, ExecutionAdmin)
 admin.site.register(ExecutionParameter)
 admin.site.register(StringType)
 admin.site.register(IntegerType)
@@ -13,4 +28,4 @@ admin.site.register(TimePeriodType)
 admin.site.register(Review)
 admin.site.register(DoubleType)
 admin.site.register(FileType)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
