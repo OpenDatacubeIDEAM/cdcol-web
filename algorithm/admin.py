dbf931c5b873880fffac95ed25b48dc73a4dc83a
Santiago Cabrera Silva <s.cabrera10@uniandes.edu.co>
2017-10-17 02:07:53 +0000

    Se modificó para que al intentar modificar un algoritmo, versión o parámetro lance una excepción de permiso denegado

--- algorithm/admin.py
+++ algorithm/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from algorithm.models import *
+from django.core.exceptions import PermissionDenied
 
 class AlgorithmAdmin(admin.ModelAdmin):
         def save_model(self, request, obj, form, change):
                 obj.user = request.user
                 if not change:
-                        super(ArticleAdmin, self).save_model(request, obj, form, change)
+                        super(ArticleAdmin, self).save_model(request, obj, form$
+                else:
+                        raise PermissionDenied
 
 admin.site.register(Algorithm, AlgorithmAdmin)
 admin.site.register(Topic)
