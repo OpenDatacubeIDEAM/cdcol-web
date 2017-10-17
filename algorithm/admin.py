914d549ad44ac1ff5cc565b58932ecf10b90c97a
Santiago Cabrera Silva <s.cabrera10@uniandes.edu.co>
2017-10-17 00:55:40 +0000

    Se modificó el admin.py de algorithm para que no permita modificar algoritmos, versiones ni parámetros

--- algorithm/admin.py
+++ algorithm/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from algorithm.models import *
 
-admin.site.register(Algorithm)
+class AlgorithmAdmin(admin.ModelAdmin):
+        def save_model(self, request, obj, form, change):
+                obj.user = request.user
+                if not change:
+                        super(ArticleAdmin, self).save_model(request, obj, form, change)
+
+admin.site.register(Algorithm, AlgorithmAdmin)
 admin.site.register(Topic)
 admin.site.register(VersionStorageUnit)
-admin.site.register(Version)
-admin.site.register(Parameter)
+admin.site.register(Version, AlgorithmAdmin)
+admin.site.register(Parameter, AlgorithmAdmin)
