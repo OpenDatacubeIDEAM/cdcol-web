8a33b2d1601059d4d9b44775326963aba923e026
Santiago Cabrera Silva <s.cabrera10@uniandes.edu.co>
2017-10-20 03:43:18 +0000

    Modificado para que el admin no pueda crear ni modificar algoritmos, ni versiones,  ni parámetros

--- algorithm/admin.py
+++ algorithm/admin.py
@@ -5,10 +5,7 @@ from django.core.exceptions import PermissionDenied
 class AlgorithmAdmin(admin.ModelAdmin):
         def save_model(self, request, obj, form, change):
                 obj.user = request.user
-                if not change:
-                        super(ArticleAdmin, self).save_model(request, obj, form, change)
-                else:
-                        raise PermissionDenied
+                raise PermissionDenied
 
 admin.site.register(Algorithm, AlgorithmAdmin)
 admin.site.register(Topic)
