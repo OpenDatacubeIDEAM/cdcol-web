1f3dc543fd05da6d76ec7f6cc089b5254311c1cc
Santiago Cabrera Silva <s.cabrera10@uniandes.edu.co>
2017-10-20 03:49:36 +0000

    Modificado para que el admin no pueda modificar o crear ejecuciones

--- execution/admin.py
+++ execution/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from execution.models import *
-
+from django.core.exceptions import PermissionDenied
 
 class ExecutionAdmin(admin.ModelAdmin):
 	list_display = (
@@ -9,6 +9,9 @@ class ExecutionAdmin(admin.ModelAdmin):
 	'finished_at')
 	ordering = ('-created_at',)
 	list_filter = ('state', 'results_available', 'created_at', 'email_sent', 'started_at', 'finished_at')
+	def save_model(self, request, obj, form, change):
+                obj.user = request.user
+                raise PermissionDenied
 
 class TaskAdmin(admin.ModelAdmin):
 	list_display = ('execution', 'state', 'uuid', 'created_at', 'updated_at', 'start_date', 'end_date', 'state_updated_at')
