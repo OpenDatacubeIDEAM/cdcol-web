307c2d5383d368a89d7b0ae8c2fdceb0e2a150de
Santiago Cabrera Silva <s.cabrera10@uniandes.edu.co>
2017-10-20 03:46:21 +0000

    Modificado para que el admin no pueda modificar o crear unidades de almacenamiento

--- storage/admin.py
+++ storage/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from storage.models import *
+from django.core.exceptions import PermissionDenied
 
-admin.site.register(StorageUnit)
+class StorageUnitAdmin(admin.ModelAdmin):
+        def save_model(self, request, obj, form, change):
+                obj.user = request.user
+                raise PermissionDenied
+
+
+
+admin.site.register(StorageUnit, StorageUnitAdmin)
