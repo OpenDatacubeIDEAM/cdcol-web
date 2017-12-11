df070dbb7ed42e52ae8e45b415e94d407f35064b
s.cabrera10 <s.cabrera10@uniandes.edu.co>
2017-12-11 20:53:52 +0000

    Ahora los thumbnails de las unidades de almacenamiento pasan directamente por nginx

--- storage/urls.py
+++ storage/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 	# ex: /storage/new
 	url(r'^new/$', views.new, name='new'),
 	# ex: /storage/1/image/imagename/download/
-	url(r'^(?P<storage_unit_id>[0-9]+)/image/(?P<image_name>.+)/download/$', views.download_image, name='download_image'),
+	url(r'^download/image/(?P<storage_unit_name>.*)/(?P<image_name>.+)$', views.download_image, name='download_image'),
 	# ex: /storage/1/image/imagename/metadata/
 	url(r'^(?P<storage_unit_id>[0-9]+)/image/(?P<image_name>.+)/metadata/$', views.download_metadata, name='download_metadata'),
 	# ex: /storage/1/[type]/download/
