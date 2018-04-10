271e41170dbfc8bfcfafa56ab9caf124e0284442
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-04-10 13:48:56 -0500

    campo alias

--- storage/models.py
+++ storage/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class StorageUnit(models.Model):
-	alias = models.CharField(max_length=200, default="Unidad")
+	alias = models.CharField(max_length=200, unique=True)
 	name = models.CharField(max_length=200, unique=True)
 	description = models.TextField()
 	description_file = models.CharField(max_length=200)
