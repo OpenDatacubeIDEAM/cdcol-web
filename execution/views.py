1f7f9d4f3cc5961b9147c2390ecc2f0f96820801
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-04-17 14:10:12 -0500

    cambiando de estado

--- execution/views.py
+++ execution/views.py
@@ -502,7 +502,7 @@ def generate_geotiff_task(request, execution_id, image_name):
 
 @login_required(login_url='/accounts/login/')
 def cancel_execution(request, execution_id):
-    
+    now = datetime.datetime.now
     Execution.objects.filter(id=execution_id).update(state='5', finished_at=now, end_date=now)
     json_request = {
         'execution_id': execution_id
