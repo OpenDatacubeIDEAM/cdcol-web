a0ae0de047d2b6be6db966babb8c5f5f40f61dde
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-04-23 13:57:18 -0500

    format bandas

--- execution/templatetags/execution_extras.py
+++ execution/templatetags/execution_extras.py
@@ -27,8 +27,8 @@ def get_storage_unit(value):
 	if "," not in value:
 		alias = StorageUnit.objects.get(name="value").alias
 	else:
-		unit_and_bands=value.split(",", 1)
-		alias = "{} ({})".format(StorageUnit.objects.get(name=unit_and_bands[0]).alias, name=unit_and_bands[1])
+		unit_and_bands=value.split(",", 2)
+		alias = "{} ({})".format(StorageUnit.objects.get(name=unit_and_bands[0]).alias, unit_and_bands[1])
 
 	return alias
 
