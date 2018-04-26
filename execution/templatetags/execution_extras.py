be82b05910ff73f06c4874f3249f4f5ef10a7050
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-04-25 19:40:50 -0500

    raw query para ver algoritmos acitovs en execution extras

--- execution/templatetags/execution_extras.py
+++ execution/templatetags/execution_extras.py
@@ -10,7 +10,7 @@ def get_algorithms(value):
 	"""Returns all the algorithms belonging to a certain topic"""
 
 	#return Algorithm.objects.filter(topic=value)
-	query = 'SELECT * FROM algorithm_algorithm AS alg LEFT JOIN algorithm_version AS ver ON alg.id = ver.id WHERE ver.publishing_state=\'1\' OR ver.publishing_state=\'2\' AND alg.topic_id={}'.format(str(value.id))
+	query = 'SELECT * FROM algorithm_algorithm AS alg LEFT JOIN algorithm_version AS ver ON alg.id = ver.id WHERE (ver.publishing_state=\'1\' OR ver.publishing_state=\'2\') AND alg.topic_id={}'.format(str(value.id))
 	return Algorithm.objects.raw(query);
 
 register.filter('get_algorithms', get_algorithms)
