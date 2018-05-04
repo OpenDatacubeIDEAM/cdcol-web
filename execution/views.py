447816c2f7a8a69dcb89de447047ce035499d78a
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-05-04 01:12:36 -0500

    Estado del task

--- execution/views.py
+++ execution/views.py
@@ -103,7 +103,10 @@ def get_detail_context(execution_id):
         for i in range(int(area_param.areatype.latitude_start), int(area_param.areatype.latitude_end)):
             for j in range(int(area_param.areatype.longitude_start), int(area_param.areatype.longitude_end)):
                 file_name= '{}_{}_{}_{}_(u{}u{})_output.nc'.format(algorithm_name, execution.version.number, i, j, time_period_param.timeperiodtype.start_date.strftime("%d-%m-%Y"), time_period_param.timeperiodtype.end_date.strftime("%d-%m-%Y"))
-                f = {'file': file_name, 'lat': i, 'long': j, 'task_state': '00', 'result_state': os._exists(system_path+file_name), 'state': False, 'tiff_file': file_name.replace('.nc', '.tiff')}
+                f = {'file': file_name, 'lat': i, 'long': j, 'task_state': '', 'result_state': os._exists(system_path+file_name), 'state': False, 'tiff_file': file_name.replace('.nc', '.tiff')}
+                if f['result_state']:
+                    f['task_state'] = 'Finalizado'
+                
                 try:
                     convertion_task = FileConvertionTask.objects.get(execution=execution, filename=f['file'])
                     f['state'] = convertion_task.state
