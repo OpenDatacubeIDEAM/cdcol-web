9ba0eca3ccf775c5e2cc749b27d162200217db82
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-05-28 23:31:21 -0500

    credits_used

--- execution/views.py
+++ execution/views.py
@@ -438,7 +438,7 @@ def new_execution(request, algorithm_id, version_id, copy_execution_id = 0):
     current_user = request.user
     credits_approved = UserProfile.objects.get(user=current_user).credits_approved
     credits_used=Execution.objects.filter(Q(executed_by=current_user), Q(state=Execution.ENQUEUED_STATE or Execution.EXECUTING_STATE)).aggregate(Sum('credits_consumed'))
-    credits_approved -= credits_used
+    credits_approved -= credits_used['credits_consumed__sum']
     algorithm = get_object_or_404(Algorithm, id=algorithm_id)
     version_selection_form = VersionSelectionForm(algorithm_id=algorithm_id, current_user=current_user)
     current_version = None
