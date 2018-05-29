cac0c98e32e4e0309236814c611839214daa0d21
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-05-28 23:54:16 -0500

    credits_used

--- execution/views.py
+++ execution/views.py
@@ -6,7 +6,6 @@ from django.core import serializers
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.db.models import Avg, Q, Sum
-from django.db.models.functions import  Coalesce
 from django.utils.encoding import smart_str
 from algorithm.models import Topic, Algorithm, VersionStorageUnit
 from execution.models import *
@@ -439,8 +438,9 @@ def new_execution(request, algorithm_id, version_id, copy_execution_id = 0):
     current_user = request.user
     credits_approved = UserProfile.objects.get(user=current_user).credits_approved
     states = [Execution.ENQUEUED_STATE, Execution.EXECUTING_STATE]
-    credits_used=Execution.objects.filter(executed_by=current_user, state__in=states).annotate(credits_consumed__sum=Coalesce(Sum('credits_consumed'),0))
-    credits_approved -= credits_used['credits_consumed__sum']
+    credits_used=Execution.objects.filter(executed_by=current_user, state__in=states).aggregate(Sum('credits_consumed'))
+    if(credits_used['credits_consumed__sum'])
+        credits_approved -= credits_used['credits_consumed__sum']
     algorithm = get_object_or_404(Algorithm, id=algorithm_id)
     version_selection_form = VersionSelectionForm(algorithm_id=algorithm_id, current_user=current_user)
     current_version = None
