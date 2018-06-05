28276ae89858552181b63b19388973321a81c8a3
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-06-05 00:49:54 -0500

    usuario en dialogo de creditos

--- public/templatetags/public_extras.py
+++ public/templatetags/public_extras.py
@@ -2,6 +2,7 @@
 from django import template
 from execution.models import Execution
 from user_profile.models import UserProfile
+from django.db.models import Sum
 # registering a new filter
 register = template.Library()
 
@@ -15,7 +16,9 @@ def has_group(user, group_name):
 @register.inclusion_tag('public/menu_executions.html')
 def get_execution(user):
 	#executions = Execution.objects.filter(state__in=[Execution.EXECUTING_STATE, Execution.ENQUEUED_STATE], cr)
-	executions = Execution.objects.filter(executed_by=user)
-	used_credits = user.id
+	executions = Execution.objects.filter(state__in=[Execution.EXECUTING_STATE, Execution.ENQUEUED_STATE],executed_by=user)
 	available_credits = UserProfile.objects.get(user=user).credits_approved
-	return {'executions': executions, 'used_credits': used_credits, 'available_credits': available_credits}
+	used_credits = executions.aggregate(Sum('credits_consumed'))
+	if used_credits['credits_consumed__sum']:
+		available_credits -= used_credits['credits_consumed__sum']
+	return {'executions': executions, 'used_credits': used_credits['credits_consumed__sum'], 'available_credits': available_credits}
