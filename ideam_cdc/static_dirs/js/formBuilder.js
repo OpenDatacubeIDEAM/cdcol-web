23942987032899f758c1182b0a3f9e7ea46ffef1
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-06-08 16:22:37 -0500

    algoritmos publicos

--- ideam_cdc/static_dirs/js/formBuilder.js
+++ ideam_cdc/static_dirs/js/formBuilder.js
@@ -187,9 +187,9 @@ $(document).ready(function () {
     function createForm(json) {
         executed_params = JSON.parse(executed_params);
         credits_approved = JSON.parse(credits_approved);
-        // storage_units_version = JSON.parse(storage_units_version);
+        version = JSON.parse(version);
         console.log(credits_approved);
-        // console.log(storage_units_version);
+        console.log(version);
         console.log(executed_params);
         // obtaining the form
         var f = document.getElementById("mainForm");
