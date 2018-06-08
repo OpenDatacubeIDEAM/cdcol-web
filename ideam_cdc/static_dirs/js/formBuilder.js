dcc9d3cdf898ea8117bc513355751d64354e2851
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-06-08 17:10:58 -0500

    algoritmos publicos

--- ideam_cdc/static_dirs/js/formBuilder.js
+++ ideam_cdc/static_dirs/js/formBuilder.js
@@ -190,8 +190,8 @@ $(document).ready(function () {
         storage_units_version = storage_units_version.substring(1, storage_units_version.length-1).split(",");
         for(var i=0; i<storage_units_version.length; i++){
             storage_units_version[i] = storage_units_version[i].split(":");
-            storage_units_version[i] = storage_units_version[i][1];
-            storage_units_version[i] = storage_units_version[i].substring(1,storage_units_version.length[i]-1);
+            // storage_units_version[i] = storage_units_version[i][1];
+            // storage_units_version[i] = storage_units_version[i].substring(1,storage_units_version.length[i]-1);
         }
 
         console.log(storage_units_version);
