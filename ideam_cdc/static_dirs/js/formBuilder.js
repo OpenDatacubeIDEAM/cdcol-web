d66c4879718d4bf63e1b6f3ad207a64d696a50af
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-06-11 18:58:48 -0500

    anhos en el backend y en frontend

--- ideam_cdc/static_dirs/js/formBuilder.js
+++ ideam_cdc/static_dirs/js/formBuilder.js
@@ -109,7 +109,7 @@ $(document).ready(function () {
                     anhos += 1
                     start_date = document.getElementById("start_date_"+time_pks[i]);
                     end_date = document.getElementById("end_date_"+time_pks[i]);
-                    if(start_date && end_date){
+                    if(start_date && end_date && start_date.includes("-") && end_date.includes("-")){
                         start_date = start_date.value.split["-"];
                         end_date = end_date.value.split["-"];
                         if(start_date.length == 3 && end_date.length == 3)
