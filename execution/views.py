4d66b995dc18338ff6b35718798c7aab8b38f51a
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-06-12 12:50:25 -0500

    pŕobando resultado

--- execution/views.py
+++ execution/views.py
@@ -129,7 +129,6 @@ def get_detail_context(execution_id):
                     f['task_state'] = 'Cancelado'
                 else:
                     f['task_state'] = 'Sin información dispónible'
-                f['task_state'] = "{}-{}".format(file_name, os.path.exists(system_path+file_name))
                 try:
                     convertion_task = FileConvertionTask.objects.get(execution=execution, filename=f['file'])
                     f['state'] = convertion_task.state
