ab5a143a257031d97291aee803197dc7094c6523
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2019-03-08 19:58:36 -0500

    modify index view to redirect admin to django interface

--- public/views.py
+++ public/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
 def index(request):
+
+	if request.user.is_superuser:
+		# Super users does not have UserProfile
+		# since they are created using the 
+		# createsuper user command that creates 
+		# User instances.
+		return redirect('admin/')
+
 	if request.user.is_authenticated():
 		return render(request, 'public/home.html')
+
 	return render(request, 'public/index.html')
 
 
