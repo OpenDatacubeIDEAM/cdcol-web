66ec4625855dcaf4f947e8999623a5e82f16e08f
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-06-07 22:22:53 -0500

    version admin

--- algorithm/admin.py
+++ algorithm/admin.py
@@ -10,6 +10,39 @@ from django.core.exceptions import PermissionDenied
 
 
 class RestrictAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        raise PermissionDenied
+
+class VersionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'number',
+        'publishing_state',
+        'version_name',
+        'algorithm_name',
+        'algorithm_created_by'
+    )
+
+    def version_name(self,obj):
+        return obj.name
+
+    version_name.short_description = 'Nombre Version'
+
+    def algorithm_name(self,obj):
+        return obj.algorithm.name
+
+    algorithm_name.short_description = 'Nombre Algorithmo'
+    algorithm_name.admin_order_field = 'algorithm__name'
+
+    def algorithm_created_by(self,obj):
+        return obj.algorithm.created_by
+
+    algorithm_created_by.short_description = 'Creado Por'
+    algorithm_created_by.admin_order_field = 'algorithm__created_by'
+
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         raise PermissionDenied
@@ -18,5 +51,5 @@ class RestrictAdmin(admin.ModelAdmin):
 admin.site.register(Topic)
 admin.site.register(VersionStorageUnit)
 admin.site.register(Algorithm,RestrictAdmin)
-admin.site.register(Version,RestrictAdmin)
+admin.site.register(Version,VersionAdmin)
 admin.site.register(Parameter,RestrictAdmin)
